8fa7f6437a862118e259d0e2a66bd5288d3b3498
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-03-17 00:29:43 +0300

    Correct config-server

--- github-config-server/src/main/java/ru/zubov/githubconfigserver/GithubConfigServerApplication.java
+++ github-config-server/src/main/java/ru/zubov/githubconfigserver/GithubConfigServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class GithubConfigServerApplication {
 
     public static void main(String[] args) {
